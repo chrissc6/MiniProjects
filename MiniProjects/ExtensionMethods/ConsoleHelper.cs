e2a2bc6da4085361947c97d6f14a732ef0a2fe0f
chris <christopherscaudill@gmail.com>
2020-05-17 06:01:19 -0400

    ExtensionMethods

---
+++ MiniProjects/ExtensionMethods/ConsoleHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string output = "";
+
+            while (string.IsNullOrWhiteSpace(output))
+            {
+                Console.WriteLine(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        public static int RequestInt(this string message)
+        {
+            return message.RequestInt(false);
+        }
+
+        public static int RequestInt(this string message, int minValue, int maxValue)
+        {
+            return message.RequestInt(true, minValue, maxValue);
+        }
+
+        private static int RequestInt(this string message, bool useMM, int minValue = 0, int maxValue = 0)
+        {
+            int output = 0;
+            bool isValidInt = false;
+            bool isInValidRange = true;
+
+            while (!isValidInt || !isInValidRange)
+            {
+                Console.WriteLine(message);
+                isValidInt = int.TryParse(Console.ReadLine(), out output);
+
+                if (useMM)
+                {
+                    //better if else
+                    isInValidRange = (output >= minValue && output <= maxValue);
+
+                    //if (output >= minValue && output <= maxValue)
+                    //{
+                    //    isInValidRange = true;
+                    //}
+                    //else
+                    //{
+                    //    isInValidRange = false;
+                    //}
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ MiniProjects/ExtensionMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            //Console.WriteLine("What is your first name?: ");
+            //person.FirstName = Console.ReadLine();
+            person.FirstName = "What is your first name?: ".RequestString();
+
+            person.LastName = "What is your last name?: ".RequestString();
+
+            //person.Age = "What is your age?: ".RequestInt();
+            person.Age = "What is your age?: ".RequestInt(0, 120);
+
+            Console.ReadLine();
+        }
+    }
+}
